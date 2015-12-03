81f75763afa56b5827067ec8f013d56ca8025194
ejwalters <ejwalters24@gmail.com>
2015-12-03 15:08:13 -0500

    change

--- expressSQL.js
+++ expressSQL.js
@@ -67,7 +67,7 @@ app.get('/',function(req,res,next){
 
 app.get('/insert',function(req,res,next){
   var context = {};
-  pool.query("INSERT INTO workouts (`name`, `reps`, `weight`) VALUES (?,?,?)", [req.query.name, req.query.reps, req.query.weight ], function(err, result){
+  pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`) VALUES (?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date ], function(err, result){
     if(err){
       next(err);
       return;
