ad76416cffa062543cb48fef23b6134c1ff8812a
ejwalters <ejwalters24@gmail.com>
2015-12-03 19:58:12 -0500

    new get

--- expressSQL.js
+++ expressSQL.js
@@ -50,7 +50,7 @@ app.get('/',function(req,res,next){
   });
 });
 
-app.post('/insert',function(req,res,next){
+app.get('/insert',function(req,res,next){
   var context = {};
   pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?,?,?,?,?)", 
   [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs ], 
